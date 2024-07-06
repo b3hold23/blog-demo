41948c461f530ac8b8798a2a1d3106a8b918ebcb
Julie Call <jolleyyy@Julies-MBP.home>
2024-07-06 00:53:18 -0600

    added content to blog.js file

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,38 @@
 // TODO: Create a variable that selects the main element
+let blogPosts = [];
+const apiEndpoint = './api/blog.json';
 
+async function getBlogPosts() {
+try {
+    
+    const response = await fetch(apiEndpoint);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch blog posts`);
+    }
+    const data = await response.json();
+    blogPosts = data;
+    displayBlogPosts(blogPosts);
+} catch (error) {
+    console.error ('fetchihng blog posts:', error);
+}
+}
+
+
+function displayBlogPosts(blogPosts) {
+    const blogContainer = document.getElementById('blogContainer');
+    blogContainer.innerHTML = '';
+
+    posts.forEach((post) => {
+        const blogPost = document.createElement('div');
+        blogPost.className = 'blogPost';
+        blogPost.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.content}</p>
+            <p>${post.author}</p>
+        `;
+        blogContainer.appendChild(blogPost);
+    });
+}
 // TODO: Create a function that builds an element and appends it to the DOM
 
 // TODO: Create a function that handles the case where there are no blog posts to display
